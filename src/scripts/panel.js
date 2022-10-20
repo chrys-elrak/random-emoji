67aa52c01f76d48997f93279846915c839de8416
Chrys Rakotonimanana <chrysrakk@gmail.com>
2022-10-20 10:51:17 +0300

    add prompt message

--- src/scripts/panel.js
+++ src/scripts/panel.js
@@ -2,6 +2,7 @@ const emojiField = document.querySelector('.emoji-field');
 const randomButton = document.querySelector('.random');
 const closeButton = document.querySelector('.close');
 const loader = document.querySelector('#loader');
+const AUTO_CLOSE_DURATION = 1000;
 
 function getEmoji() {
     return fetch('https://ranmoji.herokuapp.com/emojis/api/v.1.0/').then(res => res.json()).then(data => data.emoji);
@@ -16,11 +17,22 @@ getEmoji().then(emoji => {
 emojiField.addEventListener('click', async () => {
     // copy the emoji to the clipboard
     await navigator.clipboard.writeText(emojiField.innerText);
+    emojiField.innerHTML = `
+        <p>Copied to the clipboard</p>
+        <h2>${emojiField.innerText}</h2>
+        <i>Enjoy it!</i>
+    `;
+    emojiField.classList.add('copied-emoji');
+    closeButton.remove();
+    randomButton.remove();
     // close on click
-    window.close();
+    setTimeout(() => {
+        window.close();
+    }, AUTO_CLOSE_DURATION);
 });
 
 randomButton.addEventListener('click', async () => {
+    // show loader
     loader.classList.remove('hidden');
     emojiField.classList.add('hidden');
     // generate a new emoji
